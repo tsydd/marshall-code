d86a9db472ec703a0c003641d8019c9e1193a854
Dmitry Tsydzik <tsyd@tut.by>
2022-05-04 23:13:56 +0300

    Added page scroller

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,9 @@
 <template>
   <q-page>
     <DevicePatches />
+    <q-page-scroller :scroll-offset="150">
+      <q-btn fab icon="keyboard_arrow_up" color="primary" />
+    </q-page-scroller>
   </q-page>
 </template>
 
